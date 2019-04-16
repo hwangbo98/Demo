821b06484b87c56a038cf0cb581e60c3c44c1fc6
hwangbo98 <hwangbo1998@gmail.com>
2019-04-16 20:37:21 +0900

    edit 1

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void sub1();
+int sub1();
 void sub2();
 
 int main()
@@ -10,12 +10,6 @@ int main()
 
     return 0;
 }
-
-void sub1(){
- int a=30, b=50;
- printf("a+b=%d",a+b);
-}
-
 void sub2(){
  int a=50,b=20;
  printf("a+b=%d",a+b)
